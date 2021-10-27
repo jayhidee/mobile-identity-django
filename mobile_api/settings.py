4a1786a62d8e2c8b2a7e0ba035c5c1999623e163
Idowu John Oluwatosin <idowujohn9@gmail.com>
2021-10-27 12:47:57 +0100

    Log Fix

--- mobile_api/settings.py
+++ mobile_api/settings.py
@@ -120,11 +120,11 @@ TEMPLATES = [
 REST_FRAMEWORK = {
     #     # Use Django's standard `django.contrib.auth` permissions,
     #     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
