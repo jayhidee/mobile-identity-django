93ad9fd03c04be1ad7c1b80d337508596820332a
Idowu John Oluwatosin <idowujohn9@gmail.com>
2021-11-08 23:19:58 +0100

    Issuing Org

--- organization/urls.py
+++ organization/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import IssOrg, IssOrgD
+from .views import IssOrg, IssOrgD, IssOrgO, IssOrgDO
 
 urlpatterns = [
     path('', IssOrg.as_view()),
     path('<int:id>', IssOrgD.as_view()),
+    path('otp/', IssOrgO.as_view()),
+    path('otp/<int:id>', IssOrgDO.as_view()),
 ]
